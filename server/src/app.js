7eccad573a67401bc7b5446cd2ea20b4e0dbfab0
Jasper-Duong <duongngochung13@gmail.com>
2023-02-09 11:48:14 +0700

    npm script -> deploy, install, build, start client & server

--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,21 @@
 const express = require("express");
+const path = require("path");
 const cors = require("cors");
 const rootRouter = require("./routes/index.router");
 const app = express();
 
-app.use(cors({
-  origin: 'http://localhost:3000'
-}))
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
 
 app.use("/api", rootRouter);
+app.get("/", (req, res) => {
+  console.log('home')
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 module.exports = app;
