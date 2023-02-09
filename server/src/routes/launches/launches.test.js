31aaf2c2fd5d9881ec03f5caf1da4f1a6a6f40ce
Jasper-Duong <duongngochung13@gmail.com>
2023-02-09 23:48:49 +0700

    Add tests with jest

---
+++ server/src/routes/launches/launches.test.js
@@ -0,0 +1,12 @@
+describe("Test GET /launches", () => {
+  test("should respond with 200 success", () => {
+    const response = 200;
+    //assertion
+    expect(response).toBe(200);
+  });
+});
+describe("Test POST /launches", () => {
+  test("should respond with 201 success", () => {});
+  test("should catch null fields", () => {});
+  test("should catch invalid Dates", () => {});
+});
