45e8c8dd642c28b3460ba9f998d7a2abb1f2b14e
Jasper-Duong <duongngochung13@gmail.com>
2023-02-09 12:04:29 +0700

    Adding morgan package -> Log requests

--- server/src/routes/planets/planets.controller.js
+++ server/src/routes/planets/planets.controller.js
@@ -3,6 +3,7 @@ const { loadPlanetsData } = require("../../models/planets.model");
 async function getAllPlanets(req, res) {
   const planets = await loadPlanetsData();
   // console.log(planets);
+  console.log('getAllPlanets');
   res.status(200).json(planets);
 }
 
