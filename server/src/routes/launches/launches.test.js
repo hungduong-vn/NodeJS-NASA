e055f83b389019082a8fdd051057562f97b50425
Jasper-Duong <duongngochung13@gmail.com>
2023-02-11 11:04:23 +0700

    Add test for launches

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,12 +1,53 @@
+const request = require("supertest");
+const app = require("../../app");
+
 describe("Test GET /launches", () => {
-  test("should respond with 200 success", () => {
-    const response = 200;
-    //assertion
-    expect(response).toBe(200);
+  test("should respond with 200 success", async () => {
+    const response = await request(app)
+      .get("/api/launches")
+      .expect("Content-Type", /json/)
+      .expect(200);
+    // console.log({response});
   });
 });
 describe("Test POST /launches", () => {
-  test("should respond with 201 success", () => {});
-  test("should catch null fields", () => {});
-  test("should catch invalid Dates", () => {});
+  const testLaunch = {
+    mission: "Lunar Quest X",
+    rocket: "Thiên Long 13M",
+    target: "Kepler-186 f",
+    launchDate: "03/12/2050",
+  };
+  const makeReq = (data, statusCode = 201) =>
+    request(app)
+      .post("/api/launches")
+      .send(data)
+      .expect("Content-Type", /json/)
+      .expect(statusCode);
+  let response;
+  test("should respond with 201 success", async () => {
+    response = await makeReq(testLaunch);
+
+    const { launchDate, ...launchWithoutDate } = testLaunch;
+    expect(response.body).toMatchObject(launchWithoutDate);
+
+    const reqDate = new Date(launchDate).valueOf();
+    const resDate = new Date(response.body.launchDate).valueOf();
+    expect(reqDate).toBe(resDate);
+  });
+  test("should catch null fields", async () => {
+    for (let key in Object.keys(testLaunch)) {
+      response = await makeReq({ ...testLaunch, [key]: "" }, 400);
+      expect(response.body).toStrictEqual({
+        error: `Missing required value for ${key}`,
+      });
+    }
+
+  });
+  test("should catch invalid Dates", async () => {
+    const launchInvalidDate = {...testLaunch, launchDate:"Hello World"};
+    response = await makeReq(launchInvalidDate, 400);
+    expect(response.body).toStrictEqual({
+      error: `Invalid Date!`,
+    });
+  });
 });
