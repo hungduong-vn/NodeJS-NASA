26c195741a944fbf80b0a864385716a62909d14e
Jasper-Duong <duongngochung13@gmail.com>
2023-03-15 11:55:28 +0700

    Add launches controller & test

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -2,13 +2,16 @@ const {
   getAllLaunches,
   addLaunch,
   abortLaunch,
+  checkLaunchExist,
 } = require("../../models/launches.model");
+const { getPagination } = require("../../utils/query");
 
-function httpGetLaunches(req, res) {
-  return res.status(200).json(getAllLaunches());
+async function httpGetLaunches(req, res) {
+  const { skip, limit } = getPagination(req.query);
+  return res.status(200).json(await getAllLaunches(limit, skip));
 }
 
-function httpPostLaunch(req, res) {
+async function httpPostLaunch(req, res) {
   const launch = req.body;
   for (let i in launch) {
     if (!launch[i]) {
@@ -23,16 +26,19 @@ function httpPostLaunch(req, res) {
       error: "Invalid Date!",
     });
   }
-  addLaunch(launch);
+  await addLaunch(launch);
   return res.status(201).json(launch);
 }
 
-function httpDeleteLaunch(req, res) {
+async function httpDeleteLaunch(req, res) {
   const id = +req.params.id;
-  try {
-    abortLaunch(id);
-    return res.status(200).json({ message: `Aborted Launch ${id}` });
-  } catch (error) {
+  const isLaunchExisted = await checkLaunchExist(id);
+  if (isLaunchExisted) {
+    const aborted = await abortLaunch(id);
+    return aborted
+      ? res.status(200).json({ message: `Aborted Launch ${id}` })
+      : res.status(500).json({ error: `Launch ${id} not aborted!` });
+  } else {
     return res.status(404).json({ error: "Launch not found!" });
   }
 }
--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,53 +1,63 @@
 const request = require("supertest");
 const app = require("../../app");
+const { mongoConnect, mongoDisconnect } = require("../../utils/mongo");
 
-describe("Test GET /launches", () => {
-  test("should respond with 200 success", async () => {
-    const response = await request(app)
-      .get("/api/launches")
-      .expect("Content-Type", /json/)
-      .expect(200);
-    // console.log({response});
+// create test environment for nested test
+describe("Launches API", () => {
+  beforeAll(async () => {
+    await mongoConnect();
   });
-});
-describe("Test POST /launches", () => {
-  const testLaunch = {
-    mission: "Lunar Quest X",
-    rocket: "Thiên Long 13M",
-    target: "Kepler-186 f",
-    launchDate: "03/12/2050",
-  };
-  const makeReq = (data, statusCode = 201) =>
-    request(app)
-      .post("/api/launches")
-      .send(data)
-      .expect("Content-Type", /json/)
-      .expect(statusCode);
-  let response;
-  test("should respond with 201 success", async () => {
-    response = await makeReq(testLaunch);
-
-    const { launchDate, ...launchWithoutDate } = testLaunch;
-    expect(response.body).toMatchObject(launchWithoutDate);
-
-    const reqDate = new Date(launchDate).valueOf();
-    const resDate = new Date(response.body.launchDate).valueOf();
-    expect(reqDate).toBe(resDate);
+  // Jest auto closed connection -> No need to disconnect manually
+  // afterAll(async () => {
+  //   await mongoDisconnect();
+  // });
+  describe("Test GET /launches", () => {
+    test("should respond with 200 success", async () => {
+      const response = await request(app)
+        .get("/api/v1/launches")
+        .expect("Content-Type", /json/)
+        .expect(200);
+      // console.log({response});
+    });
   });
-  test("should catch null fields", async () => {
-    for (let key in Object.keys(testLaunch)) {
-      response = await makeReq({ ...testLaunch, [key]: "" }, 400);
+  describe("Test POST /launches", () => {
+    const testLaunch = {
+      mission: "Lunar Quest X",
+      rocket: "Thiên Long 13M",
+      target: "Kepler-186 f",
+      launchDate: "03/12/2050",
+    };
+    const makeReq = (data, statusCode = 201) =>
+      request(app)
+        .post("/api/v1/launches")
+        .send(data)
+        .expect("Content-Type", /json/)
+        .expect(statusCode);
+    let response;
+    test("should respond with 201 success", async () => {
+      response = await makeReq(testLaunch);
+
+      const { launchDate, ...launchWithoutDate } = testLaunch;
+      expect(response.body).toMatchObject(launchWithoutDate);
+
+      const reqDate = new Date(launchDate).valueOf();
+      const resDate = new Date(response.body.launchDate).valueOf();
+      expect(reqDate).toBe(resDate);
+    });
+    test("should catch null fields", async () => {
+      for (let key in Object.keys(testLaunch)) {
+        response = await makeReq({ ...testLaunch, [key]: "" }, 400);
+        expect(response.body).toStrictEqual({
+          error: `Missing required value for ${key}`,
+        });
+      }
+    });
+    test("should catch invalid Dates", async () => {
+      const launchInvalidDate = { ...testLaunch, launchDate: "Hello World" };
+      response = await makeReq(launchInvalidDate, 400);
       expect(response.body).toStrictEqual({
-        error: `Missing required value for ${key}`,
+        error: `Invalid Date!`,
       });
-    }
-
-  });
-  test("should catch invalid Dates", async () => {
-    const launchInvalidDate = {...testLaunch, launchDate:"Hello World"};
-    response = await makeReq(launchInvalidDate, 400);
-    expect(response.body).toStrictEqual({
-      error: `Invalid Date!`,
     });
   });
 });
