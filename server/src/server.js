488af7cc74728800de998fe47b4b36fc5c2f7439
Jasper-Duong <duongngochung13@gmail.com>
2023-03-15 11:52:12 +0700

    Load planets & launch data on serverStart

--- server/src/server.js
+++ server/src/server.js
@@ -1,27 +1,19 @@
 const http = require("http");
 const app = require("./app");
-// const { APP_PORT } = require('./configs/index.configs');
-const mongoose = require("mongoose");
-require('dotenv').config();
-
-console.log(process.env.MONGO_URL);
-
-const MONGO_URL = process.env.MONGO_URL;
-const PORT = 8080;
+require("dotenv").config();
+const { loadLaunchData } = require("./models/launches.model");
+const { loadPlanetsData } = require("./models/planets.model");
+const { mongoConnect } = require("./utils/mongo");
+const PORT = process.env.PORT || 8080;
 
 const server = http.createServer(app);
 
-mongoose.connection.once("open", () => {
-  console.log("MongoDB connection ready!");
-});
-mongoose.connection.on("error", (err) => {
-  console.error(err);
-});
-
 async function startServer() {
-  await mongoose.connect(MONGO_URL);
+  await mongoConnect();
   server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);
   });
+  loadPlanetsData();
+  loadLaunchData();
 }
 startServer();
